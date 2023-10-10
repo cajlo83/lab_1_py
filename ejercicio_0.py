c56921f0c0aa3831026940644ba48f3825a9511b
cajlo83 <cee.mm96@gmail.com>
2023-10-10 06:08:46 -0300

    empezando stark 5

--- ejercicio_0.py
+++
@@ -1,8 +0,0 @@
-numero = input("ingrese un numero; ")
-numero2 = input("ingrese otro numero; ")
-
-numero = int(numero)
-numero2 = int(numero2)
-
-print(numero + numero2)
-print("la suma de " + str(numero) + " + " + str(numero2) + " es: " + str(numero + numero2))
